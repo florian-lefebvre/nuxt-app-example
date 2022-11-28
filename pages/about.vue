528a8036f609408a4bdbb31ff10aa02effe656de
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-11-28 13:50:55 +0100

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const counter = ref(0);
+const increment = () => counter.value++;
+const decrement = () => counter.value--;
+</script>
+
+<template>
+  <div>
+    <h1>About page</h1>
+    <NuxtLink to="/">Home</NuxtLink>
+    <p>Counter: {{ counter }}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Home page</h1>
+  <NuxtLink to="/about">About</NuxtLink>
+</template>
